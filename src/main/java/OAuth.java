377f3582b06740268eba74672547843d55efe834
GreenLantern101 <arctangent20@yahoo.com>
2017-07-20 12:18:11 -0500

    Refactor OAuth server-side flow

--- src/main/java/OAuth.java
+++ src/main/java/OAuth.java
@@ -6,9 +6,9 @@ import io.vertx.ext.auth.oauth2.OAuth2ClientOptions;
 import io.vertx.ext.auth.oauth2.providers.GoogleAuth;
 
 public class OAuth {
-
-    public static String verify(String idTokenString, Vertx vertx) {
-
+    private OAuth2Auth oauth2;
+    private String authorization_uri;
+    public OAuth(Vertx vertx){
         OAuth2ClientOptions credentials = new OAuth2ClientOptions()
                 .setClientID("<client-id>")
                 .setClientSecret("<client-secret>")
@@ -19,16 +19,19 @@ public class OAuth {
         /*http://vertx.io/docs/apidocs/io/vertx/ext/auth/oauth2/providers/GoogleAuth.html*/
         String clientId = "";
         String clientSecret = "";
-        OAuth2Auth oauth2 = GoogleAuth.create(vertx, clientId, clientSecret);
+        this.oauth2 = GoogleAuth.create(vertx, clientId, clientSecret);
 
         // Authorization oauth2 URI
-        String authorization_uri = oauth2.authorizeURL(new JsonObject()
+        this.authorization_uri = oauth2.authorizeURL(new JsonObject()
                 .put("redirect_uri", "http://localhost:8080/callback")
                 .put("scope", "<scope>")
                 .put("state", "<state>"));
+    }
+
+    public String verify(String idTokenString, Vertx vertx) {
 
         // Redirect example using Vert.x
-        response.putHeader("Location", authorization_uri)
+        response.putHeader("Location", this.authorization_uri)
                 .setStatusCode(302)
                 .end();
 
@@ -38,10 +41,17 @@ public class OAuth {
 
         // Callbacks
         // Save the access token
-        oauth2.getToken(tokenConfig, res -> {
+        this.oauth2.getToken(tokenConfig, res -> {
             if (res.failed()) {
                 System.err.println("Access Token Error: " + res.cause().getMessage());
             } else {
+
+                /* check only @vanderbilt.edu emails
+                The hd (hosted domain) parameter streamlines the login process for G Suite hosted
+                   accounts. By including the domain of the G Suite user (for example, mycollege.edu)
+                   */
+
+
                 // Get the access token object
                 // (the authorization code is given from the previous step).
                 AccessToken token = res.result();
