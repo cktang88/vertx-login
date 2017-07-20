e1e33f5378c53fe46aeda180219c6820072698f9
GreenLantern101 <arctangent20@yahoo.com>
2017-07-19 21:19:49 -0500

    Add basic server

--- src/main/java/server.java
+++ src/main/java/server.java
@@ -1,4 +1,21 @@
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Future;
 
-public class server {
-    
+public class server extends AbstractVerticle {
+
+    @Override
+    public void start(Future<Void> fut) {
+        vertx.createHttpServer()
+            .requestHandler(r -> {
+                r.response().end("<h1>Hello from my first " +
+                        "Vert.x 3 application</h1>");
+            })
+            .listen(8080, result -> {
+                if (result.succeeded()) {
+                    fut.complete();
+                } else {
+                    fut.fail(result.cause());
+                }
+            });
+    }
 }
