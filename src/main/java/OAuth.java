d35860fc60213c7656f3a85c2e316215bd8a77fc
GreenLantern101 <arctangent20@yahoo.com>
2017-07-20 20:04:25 -0500

    Verify Google OAuth tokens

--- src/main/java/OAuth.java
+++ src/main/java/OAuth.java
@@ -6,6 +6,10 @@ import io.vertx.ext.auth.oauth2.OAuth2Auth;
 import io.vertx.ext.auth.oauth2.OAuth2ClientOptions;
 import io.vertx.ext.auth.oauth2.providers.GoogleAuth;
 
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken.Payload;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
+
 public class OAuth {
     private OAuth2Auth oauth2;
     private String authorization_uri;
@@ -25,7 +29,7 @@ public class OAuth {
         System.out.println("OAuth2 initialized.");
     }
 
-    public String verify(String idTokenString, HttpServerResponse response) {
+    public String process(String idTokenString, HttpServerResponse response) {
 
         // Redirect example using Vert.x
         response.putHeader("Location", this.authorization_uri)
@@ -34,7 +38,7 @@ public class OAuth {
 
         JsonObject tokenConfig = new JsonObject()
                 .put("code", "<code>")
-                .put("redirect_uri", "http://localhost:3000/callback");
+                .put("redirect_uri", "/webroot/home.html");
 
         // Callbacks
         // Save the access token
@@ -43,17 +47,69 @@ public class OAuth {
                 System.err.println("Access Token Error: " + res.cause().getMessage());
             } else {
 
-                /* check only @vanderbilt.edu emails
-                The hd (hosted domain) parameter streamlines the login process for G Suite hosted
-                   accounts. By including the domain of the G Suite user (for example, mycollege.edu)
-                   */
-
-
                 // Get the access token object
                 // (the authorization code is given from the previous step).
                 AccessToken token = res.result();
             }
         });
         return "";
+    }
+
+    private void verify(String idTokenString){
+        /*
+        To validate token, check the following:
+        1. The ID token is properly signed by Google. Use Google's public keys
+        (available in JWK or PEM format) to verify the token's signature.
+        2. The value of "aud" in the ID token is equal to one of your app's client IDs.
+        This check is necessary to prevent ID tokens issued to a malicious app being used to
+        access data about the same user on your app's backend server.
+        3. The value of "iss" in the ID token is equal to accounts.google.com
+        or https://accounts.google.com.
+        4. The expiry time ("exp") of the ID token has not passed.
+        5. If you want to restrict access to only members of your G Suite domain,
+        verify that the ID token has an hd claim that matches your G Suite domain name.
+
+        src: https://developers.google.com/identity/sign-in/web/backend-auth
+        NOTE: this is all conveniently handled using GoogleIdTokenVerifier
+        */
+
+        GoogleIdTokenVerifier verifier = new GoogleIdTokenVerifier.Builder(transport, jsonFactory)
+                .setAudience(Collections.singletonList(CLIENT_ID))
+                // Or, if multiple clients access the backend:
+                //.setAudience(Arrays.asList(CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3))
+                .build();
+
+// (Receive idTokenString by HTTPS POST)
+
+        GoogleIdToken idToken = verifier.verify(idTokenString);
+        if (idToken != null) {
+            Payload payload = idToken.getPayload();
+
+            // Print user identifier
+            String userId = payload.getSubject();
+            System.out.println("User ID: " + userId);
+
+            // Get profile information from payload
+            String email = payload.getEmail();
+            boolean emailVerified = Boolean.valueOf(payload.getEmailVerified());
+            String name = (String) payload.get("name");
+            String pictureUrl = (String) payload.get("picture");
+            String locale = (String) payload.get("locale");
+            String familyName = (String) payload.get("family_name");
+            String givenName = (String) payload.get("given_name");
+
+            // Use or store profile information
+            // ...
+
+            /* allow only @vanderbilt.edu emails
+               The hd (hosted domain) parameter streamlines the login process for G Suite hosted
+               accounts. By including the domain of the G Suite user (for example, mycollege.edu)
+             */
+
+            // payload.getHostedDomain()
+
+        } else {
+            System.out.println("Invalid ID token.");
+        }
     }
 }
